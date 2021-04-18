3e4d612f0042d399ac0932f9ea8a8d8d7585e7da
Petr Filip <petr.filip@plus4u.net>
2021-04-18 19:49:53 +0200

    TODO list implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const todos = [
+    {
+      id: 0,
+      text: "Vytvořit todo list v Reactu"
+    },
+    {
+      id: 1,
+      text: "Nahrát na server aplikaci"
+    }
+  ]
+
+  const [todoList, setTodoList] = useState(todos);
+  const [input, setInput] = useState("")
+
+  const addHandler = () => {
+    const newTodoList = [...todoList];
+
+    const newTodo = {
+      id: new Date().getTime(),
+      text: input
+    }
+
+    newTodoList.push(newTodo)
+    setTodoList(newTodoList);
+    setInput("")
+  }
+
+  const removeHandler = (todoId) => {
+    const filtered = todoList.filter(item => item.id !== todoId)
+    setTodoList(filtered)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Nový úkol:
+        <input type={"text"} value={input} onChange={(event) => setInput(event.target.value)}/>
+        <button onClick={addHandler}>Přidat</button>
+      </div>
+      <hr/>
+
+      {todoList.map(item => <div className={"todo-item"}>
+        <div key={item.id}>{item.text}</div>
+        <button onClick={() => {
+          removeHandler(item.id)
+        }}>Hotovo
+        </button>
+      </div>)}
+      {todoList.length === 0 && "Vše je hotovo"}
+
     </div>
   );
 }
